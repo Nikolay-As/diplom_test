c1cd8f2eaf3c97759689d1811e25897362d0e7c0
Nikolay <nikolay.popurey@yandex.ru>
2024-05-10 20:28:59 +0300

    подключение бд

--- socket_client.js
+++ socket_client.js
@@ -1,22 +1,42 @@
-const io = require("socket.io-client");
+// const io = require("socket.io-client");
 
 // Если локалхост то не используем url_socket_server
 // иначе const socket = io(url_socket_server);
-const port = 3001;
-const url_socket_server = "http://192.168.0.16:" + port;
-var socket = io(url_socket_server, { reconnect: true });
+// const port = 3001;
+// const url_socket_server = "http://192.168.0.16:" + port;
+// var socket = io(url_socket_server, { reconnect: true });
 
 const led_pin = 575; // gpio 4
 const gpio = require("onoff").Gpio; // Подключаем библиотеку для работы с gpio
 const led = new gpio(led_pin, "out");
+const sqlite3 = require('sqlite3')
+const dbFilePath = 'box.db';
 
-socket.on("connect", () => {
-  socket.emit("authorization",{id : 1})
-  console.log("передал успешно id");
+// socket.on("connect", () => {
+//   socket.emit("authorization",{id : 1})
+//   console.log("передал успешно id");
+// });
+
+// socket.on("hello_world", () => {
+//   console.log(led.readSync());
+//   led.writeSync(led.readSync() ^ 1);
+// });
+
+let db = new sqlite3.Database(dbFilePath, (err) => {
+  if (err) {
+    console.error(err.message);
+  }
+  console.log('Connected to the chinook database.');
 });
 
-socket.on("hello_world", () => {
-  console.log(led.readSync());
-  led.writeSync(led.readSync() ^ 1);
-});
+function open_door(number_door){
 
+}
+
+function close_door(number_door){
+  
+}
+
+function connect_sql(){
+  
+}
