48f4ba7c81c1e0bafee853cd7d4c7e58a90e8719
Nikolay <nikolay.popurey@yandex.ru>
2024-05-13 02:35:03 +0300

    Функция включения/отключения лампочки

--- socket_client.js
+++ socket_client.js
@@ -13,7 +13,7 @@ let door_info_pin = new Array(); // тут хранится информация
 
 door_info_pin = git_info_at_start();
 if (door_info_pin.length != 0) {
-  console.log("Приложение  готово к работе!");
+  console.log("Велобокс  готово к работе!");
  led_bike_free_on(0);
  led_bike_busy_off(0);
 
